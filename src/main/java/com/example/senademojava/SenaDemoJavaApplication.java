e086b65157cb61215795447b08681f25940c5bd9
lafvg12 <lafvg12@hotmail.com>
2024-05-22 08:55:13 -0500

    Config: First commit at intellij IDEA

---
+++ src/main/java/com/example/senademojava/SenaDemoJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.senademojava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SenaDemoJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SenaDemoJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/senademojava/SenaDemoJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.senademojava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SenaDemoJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
